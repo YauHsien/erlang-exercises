2eba2e702a7a7016a4b92ee929d03ab67ed5aa22
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2018-07-17 02:05:09 +0800

    first commit

---
+++ erlang_org_course_exercises.erl
@@ -0,0 +1,25 @@
+-module(erlang_org_course_exercises).
+-export([double/1]).
+
+%% Problem set: http://erlang.org/course/exercises.html
+%%
+%%
+
+%%
+%% Entering a program
+%%
+%% Type the demo:double example into a file called demo.erl. Use your favourite text editor.
+%%
+%% Start Erlang.
+%%
+%% Give the command c:c(demo). to compile the file.
+%%
+%% Try running the query:
+%%
+%%     demo:double(12).
+%%
+%% This is just to test if you can get the system started and can use the editor together with the Erlang system.
+%%
+double(X) ->
+    X * 2.
+
