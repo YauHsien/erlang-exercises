d3338023363697d3d705b4e59ac532b2a082d4dc
Yau-Hsien Huang <yauhsienhuang@gmail.com>
2021-06-09 15:07:43 +0800

    修正八皇后棋盤之產圖程式

--- 8-queens/draw.sh
+++ 8-queens/draw.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-erl -noshell -eval "wx:new(),io:fwrite(\"~p~n\",[image_list:generate_image('puzzle-8-queens':queens(), 3, 0.5, \"result.png\")]),wx:destroy(),halt()"
+mkdir -p solutions
+erl -noshell -eval "wx:new(),image_list:generate_image('puzzle-8-queens':queens(), 3, \"solutions/result-~p.png\"),wx:destroy(),halt()"
